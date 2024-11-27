f9615e91bfa10a663d5bf28f3ae1f518a3f7ee12
Rymedero <rymedero.21@est.ucab.edu.ve>
2024-11-26 21:10:42 -0400

    feature[Req10-10.3]:Modificar proveedor

---
+++ ProviderMS.Application/Validators/ModifyProveedorValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using ProviderMS.Commons.Dtos.Request;
+
+namespace ProviderMS.Application.Validators
+{
+    public class ModifyProveedorValidator : ValidatorBase<ModifyProveedorDto>
+    {
+        public ModifyProveedorValidator()
+        {
+
+            RuleFor(p => p.Id)
+                .NotEmpty()
+                .WithMessage("El id del proveedor es requerido.");
+
+            RuleFor(p => p.DenominacionComercial)
+                .NotEmpty()
+                .WithMessage("La denominación comercial es requerida.");
+
+            RuleFor(p => p.RazonSocial)
+                .NotEmpty()
+                .WithMessage("La razón social es requerida.");
+
+            RuleFor(p => p.DireccionFisica)
+                .NotEmpty()
+                .WithMessage("La dirección física es requerida.");
+
+            RuleFor(p => p.TipoDocumentoIdentidad)
+                .NotEmpty()
+                .WithMessage("El tipo de documento de identidad es requerido.");
+
+            RuleFor(p => p.NumeroDocumentoIdentidad)
+                .NotEmpty()
+                .WithMessage("El número del documento de identidad es requerido.");
+        }
+    }
+    
+}
---
+++ ProviderMS.Commons/Dtos/Request/ModifyProveedorDto.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ProviderMS.Commons.Dtos.Request
+{
+    public class ModifyProveedorDto
+    {
+        public Guid Id { get; set; }
+        public string DenominacionComercial { get; set; }
+        public string RazonSocial { get; set; }
+        public string DireccionFisica { get; set; }
+        public string TipoDocumentoIdentidad { get; set; }
+        public string NumeroDocumentoIdentidad { get; set; }
+
+        public ModifyProveedorDto() { }
+
+    }
+
+}
