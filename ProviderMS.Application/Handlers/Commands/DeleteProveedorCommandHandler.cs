3a13d80b02833171e883710ca5b08842bb92b97c
Rymedero <rymedero.21@est.ucab.edu.ve>
2024-11-27 18:20:53 -0400

    feature[Req10-10.4]:Eliminar Proveedor

---
+++ ProviderMS.Application/Handlers/Commands/DeleteProveedorCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using ProviderMS.Application.Commands;
+using ProviderMS.Core.Repositories;
+
+
+
+namespace ProviderMS.Application.Handlers.Commands
+{
+    public class DeleteProveedorCommandHandler : IRequestHandler<DeleteProveedorCommand, Unit>
+    {
+
+        private readonly IProveedorRepository _proveedorRepository;
+
+        public DeleteProveedorCommandHandler(IProveedorRepository proveedorRepository)
+        {
+            _proveedorRepository = proveedorRepository;
+        }
+        public async Task<Unit> Handle(DeleteProveedorCommand request, CancellationToken cancellationToken)
+        {
+            await _proveedorRepository.DeleteAsync(request.Id);
+            return Unit.Value;
+        }
+
+    }
+}
